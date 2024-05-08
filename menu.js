55aec911c6e0261453c1c5d1520903c82ef5874b
diihgab <dihgab20@gmail.com>
2024-05-08 11:29:08 -0400

    feat: add futter, responsive and menu

---
+++ menu.js
@@ -0,0 +1,10 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click',()=>{
+  menu.classList.add('abrir-menu')
+})
+
+btnMenu.addEventListener('click',()=>{
+  menu.classList.remove('abrir-menu')
+})
