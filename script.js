35c52db9969aaa5005f292a7ce7e4323e1db2682
diihgab <dihgab20@gmail.com>
2024-05-08 22:17:19 -0400

    feat: add button WPP

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
       window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 });
+
+// Quando o documento estiver pronto
+document.addEventListener("DOMContentLoaded", function() {
+  // Adicionar um evento de envio ao formulário
+  document.getElementById("formWhatsapp").addEventListener("submit", function(event) {
+      // Impedir o envio do formulário padrão
+      event.preventDefault();
+
+      // Número de telefone para envio da mensagem
+      var phoneNumber = "+5592992612577";
+
+      // Obter os valores do nome e da mensagem
+      var name = document.getElementById("name").value;
+      var message = document.getElementById("message").value;
+
+      // Formatar a mensagem para a URL do WhatsApp
+      var messageFormatted = encodeURIComponent("Nome: " + name + "\nMensagem: " + message);
+
+      // Abrir a URL do WhatsApp com o número de telefone e a mensagem
+      window.open("https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + messageFormatted, "_blank");
+  });
+});
+
+
+
+
